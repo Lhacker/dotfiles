d61bd7fb205686c0a793d7c950cbfa7e8c8ad08a
Lhacker <starclown1@gmail.com>
2014-12-30 22:46:30 +0900

    modify path

--- linux/init_common_settings.sh
+++ linux/init_common_settings.sh
@@ -9,14 +9,14 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 
 # put setting files
 cp $CURRENT_DIR/.vimrc ~/.vimrc
-cp $CURRENT_DIR/../.gitconfig ~/.gitconfig
-cp $CURRENT_DIR/../.agignore ~/.agignore
+cp $CURRENT_DIR/.gitconfig ~/.gitconfig
+cp $CURRENT_DIR/.agignore ~/.agignore
 
 # for vim
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-vim -c NeoBundleInstall
-
 mkdir -p ~/.vim/colors
 git clone https://github.com/tomasr/molokai.git $WORK_DIRECTORY/molokai
 cp $WORK_DIRECTORY/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+vim -c NeoBundleInstall
