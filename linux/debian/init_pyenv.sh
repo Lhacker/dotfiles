8f3a7f1133a26373f5aafc334feeb9d1c94872f8
Lhacker <starclown1@gmail.com>
2015-07-22 00:38:10 +0900

    add require packages for build pyenv, rbenv

--- linux/debian/init_pyenv.sh
+++ linux/debian/init_pyenv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# require packages for build
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm
+
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 echo '' >> ~/.bash_profile
 echo '#pyenv' >> ~/.bash_profile
--- linux/debian/init_rbenv.sh
+++ linux/debian/init_rbenv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# requirement packages for build
+sudo apt-get install -y zlib1g-dev build-essential libssl-dev libreadline-dev git
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo '' >> ~/.bash_profile
